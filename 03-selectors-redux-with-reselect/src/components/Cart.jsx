dc421b8b460da0c0384a523fa82273760fec607f
Abner Willys <abwillys001@gmail.com>
2022-11-14 01:01:09 -0300

    selector without reselect library

--- 03-selectors-redux-with-reselect/src/components/Cart.jsx
+++ 03-selectors-redux-with-reselect/src/components/Cart.jsx
@@ -7,6 +7,7 @@ import * as cartActions from "../redux/actions/cart"
 
 const Cart = ({
   cart,
+  total,
   addProduct,
   setShipping,
 }) => (
@@ -20,7 +21,7 @@ const Cart = ({
       Shipping Value: <strong>{cart.shipping_value}</strong>
     </p>
     <p>
-      Total: <strong>601</strong>
+      Total: <strong>{total}</strong>
     </p>
 
     <button onClick={addProduct}>Add Product</button>
@@ -28,9 +29,20 @@ const Cart = ({
   </div>
 )
 
+// Essa função já é um Selector
+const calculateTotal = (items) => {
+  // Quando calcular o frete esse log vai disparar. Isso é ruim para performance, 
+  // pois o total nao tem relação com o frete. Devido a isso utilizamos o reselect.
+  console.log("CALCULOU")
+
+  return (items.reduce((subtotal, item) => subtotal + item.price, 0))
+}
+
 const mapStateToProps = state => ({
-  cart: state.cart
+  cart: state.cart,
+  total: calculateTotal(state.cart.items)
 })
+
 const mapDispatchToProps = dispatch => bindActionCreators(cartActions, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart)
